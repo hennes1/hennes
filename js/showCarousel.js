bb5be7afc47c8f9c85655fad38c173454b752780
huanghongjun <huanghj@yft.com>
2016-12-03 11:08:04 +0800

    task#1 新单页，封装Carousel

---
+++ js/showCarousel.js
@@ -0,0 +1,117 @@
+/*!
+ * showCarousel：封装Carousel
+ * @author：Hennes
+ * @date  ：2016-12-01 19:55
+ */
+(function ($) {
+    $.fn.showCarousel = function (options) {
+        var Opts = $.extend({
+            asBg: true, //背景图滚动，默认为背景
+            asText: false, //文字和介绍，默认为无
+            slide: [
+                {
+                    image: undefined,
+                    active: false,
+                    bgColor: undefined,
+                    url: undefined,
+                    title: undefined,
+                    content: undefined
+                }
+            ]
+        }, options || {});
+
+        return this.each(function () {
+            var that = this, id = $(that).attr('id');
+            var $slide = $('<div class="carousel-inner" />'), $numPoint = $('<ol class="carousel-indicators"/>');
+
+            //上一个 下一个
+            var $PrevAndNext = $('<a class="left carousel-control" href="#'+ id +'" data-slide="prev"><span class="glyphicons glyphicons-chevron-left"></span><span class="sr-only">Previous</span><a class="right carousel-control" href="#'+ id +'" data-slide="next"><span class="glyphicons glyphicons-chevron-right"></span><span class="sr-only">Next</span></a>');
+
+            $slide.appendTo($(that));
+            $numPoint.appendTo($(that));
+            $PrevAndNext.appendTo($(that));
+
+            var showBg = Opts.asBg, showTx = Opts.asText;
+
+            //判断数据
+            var appendHtml = function (data) {
+                var html = '';
+                var txHtml = '<div class="carousel-caption">'
+                        + '       <h1>' + data.title + '</h1>'
+                        + '       <p>' + data.content + '</p>'
+                        + '       <p><a class="btn btn-lg btn-primary" href="' + data.url + '" role="button">' + data.title + '</a></p>'
+                        + '   </div>';
+                var imgBg = '<div class="item" style="background: ' + data.bgColor + ' url(' + data.image + ') 50% 0 no-repeat;">';
+                var image = '<img src="' + data.image + '">';
+
+                //作为背景
+                if (showBg === true) {
+                    if(showTx === false){ //无文字
+                        html = ''+ imgBg +'</div>';
+                    }else{ //有文字
+                        html = ''+ imgBg +''+ txHtml +'</div>';
+                    }
+                }else{ //作为图片
+                    if(showTx === false){ //无文字
+                        html = '<div class="item">'+ image +'</div>';
+                    }else{ //有文字
+                        html = '<div class="item">'+ image +''+ txHtml +'</div>';
+                    }
+                }
+                return html;
+            };
+
+            var actived = 0; // 判断第几个激活
+
+            $.each(Opts.slide, function (i, val) {
+                var cls = val.active === true ? 'active' : '';
+                if (val.active === true) actived = i;
+
+                //装载小圆点
+                var $li = $('<li data-target="#' + id + '" data-slide-to="' + i + '" />').attr('class', cls).appendTo($numPoint);
+
+                //装载滚动内容
+                var slideData = appendHtml(val);
+                $(slideData).addClass(cls).appendTo($slide);
+            });
+        });
+    }
+})(jQuery);
+
+//页面数据
+function singleData(e) {
+    var $wrap = $(e), html = '';
+
+    $.getJSON('../json/singleData.json', function (data) {
+        for(var i = 0, len = data.length; i < len; i++) {
+            html += '<div class="section-item section-item'+ (i+1) +'">'
+                +' <div class="container">'
+                + '<div class="page-header">'
+                +'     <h3>'+ data[i].column +'</h3>'
+                +' </div>'
+                +' <div class="row">';
+            for(var m = 0; m < data[i].list.length; m++) {
+                html += '<div class="col-xs-6 col-sm-6 col-md-3">'
+                    + '    <div class="thumbnail">'
+                    + '        <a href="'+ data[i].list[m].url +'">'
+                    + '            <img src="'+ data[i].list[m].image +'" alt="">'
+                    + '        </a>'
+                    + '        <div class="caption">'
+                    + '            <h4><a href="'+ data[i].list[m].url +'">'+ data[i].list[m].title +'</a></h4>'
+                    + '            <p class="text-muted">'
+                    + '                主讲人：'+ data[i].list[m].lector +'<a href="'+ data[i].list[m].url +'" class="pull-right text-primary" target="_blank">'+ data[i].list[m].other +'</a>'
+                    + '            </p>'
+                    + '        </div>'
+                    + '    </div>'
+                    + ' </div>';
+            }
+
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+        }
+
+        //添加数据
+        $wrap.append(html).removeClass('loading');
+    });
+}

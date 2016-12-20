88aa62dcb0ade0931dad4919c643e05ba5789df5
huanghongjun <huanghj@yft.com>
2016-12-20 16:28:27 +0800

    bug#7 弹层页面优化

--- js/pages/layer.js
+++ js/pages/layer.js
@@ -1,7 +1,7 @@
 /**
  * Created by hennes on 2016/11/26.
  */
-seajs.use(['jquery', 'bootstrap', 'layer', 'ZeroClipboard', 'common'], function () {
+seajs.use(['jquery', 'bootstrap', 'layer', 'common'], function () {
     //弹层CSS路径
     layer.config({
         path: '../js/plugins/layer/'
@@ -18,58 +18,15 @@ seajs.use(['jquery', 'bootstrap', 'layer', 'ZeroClipboard', 'common'], function 
 
         var $tx = $('#J_test'), txVal = $tx.val();
 
-        $('button[data-target]').on('click', function () {
-            var $self = $(this), id = $self.data('target'), $target = $('#' + id), $tr = $self.parents('tr');
-            var h = $self.data('height'), hgt = (h === '' || typeof h === 'undefined') ? 'auto' : h + 'px';
-            if($target.length > 0) {
-                layer.open({
-                    type: 1,
-                    title: id + '代码示例',
-                    move: false,
-                    area: ['800px', hgt],
-                    content: $target,
-                    success: function (layero, index) {
-                        $tr.addClass('selected');
-                    },
-                    cancel: function () {
-                        $tr.removeClass('selected');
-                    }
-                });
-            }
-        });
-
-        //在线调试
-        var popDebug = function(){
-            layer.open({
+        //定义弹窗，默认最小化右下角打开
+        var layerOpen = function (options) {
+            var opt = $.extend({
                 type: 1,
                 title: '<b>在线调试</b>',
-                id: 'Lay_layer_debug',
-                content: $('#test_lay'),
-                area: ['480px'],
-                skin: 'goTo-debug-max',
-                shade: false,
-                offset: 'rb',
-                resize: false,
-                success: function(layero, index){
-                    layero.find('.layui-layer-close1').html('<cite />');
-                    layer.style(index, {
-                        marginLeft: -10,
-                        marginTop: -10
-                    });
-                },
-                end: function () {
-                    smallDebug();
-                    if ($tx.val() == '') $tx.val(txVal);
-                }
-            });
-            J_test.focus();
-        };
-
-        //最小窗口
-        var smallDebug = function () {
-            layer.open({
-                type: 1,
-                title: '<b>在线调试</b>',
+                id: '',
+                content: '',
+                area: ['auto'],
+                move: '.layui-layer-title',
                 skin: 'goTo-debug-min',
                 shade: false,
                 offset: 'rb',
@@ -81,16 +38,86 @@ seajs.use(['jquery', 'bootstrap', 'layer', 'ZeroClipboard', 'common'], function 
                     });
                 },
                 end: function () {
-                    popDebug();
+                    layerOpen({
+                        id: 'Lay_layer_debug',
+                        content: $('#test_lay'),
+                        area: ['480px'],
+                        skin: 'goTo-debug-max',
+                        success: function(layero, index){
+                            layero.find('.layui-layer-close1').html('<cite />');
+                            layer.style(index, {
+                                marginLeft: -10,
+                                marginTop: -10
+                            });
+                        },
+                        end: function () {
+                            layerOpen();
+                            if ($tx.val() == '') $tx.val(txVal);
+                        }
+                    });
                     if ($tx.val() == '') $tx.val(txVal);
-                }
+                },
+                cancel: null
+            }, options || {});
+            layer.open(opt);
+        };
+
+        //目录跳转层
+        var dir = function () {
+            layerOpen({
+                title: '目录',
+                area: ['180px', '320px'],
+                content: $('#site_dir'),
+                skin: 'dir-layer',
+                offset: 'r',
+                success: function(layero, index){
+                    layer.style(index, {
+                        marginLeft: -10
+                    });
+                },
+                end: null,
+                cancel: null
             });
         };
-        smallDebug();
+        dir();
+
+        //显示当前状态
+        $('.site-dir li a').on('click', function () {
+            var $self = $(this), $li = $self.parent('li'), id = $self.attr('href').replace('#', '').replace('.', '_');
+            $self.parents('.site-dir').find('li').removeClass('active');
+            $li.addClass('active');
+            $('html,body').animate({scrollTop: $('#' + id).offset().top - 61}, 500);
+        });
+
+        $('button[data-target]').on('click', function () {
+            var $self = $(this), id = $self.data('target'), $target = $('#lay_' + id), $tr = $self.parents('tr');
+            var h = $self.data('height'), hgt = (h === '' || typeof h === 'undefined') ? 'auto' : h + 'px';
+            if($target.length > 0) {
+                layerOpen({
+                    title: id + '代码示例',
+                    move: false,
+                    area: ['800px', hgt],
+                    shade: 0.3,
+                    content: $target,
+                    skin: '',
+                    offset: 'auto',
+                    success: function (layero, index) {
+                        $tr.addClass('selected');
+                    },
+                    end: null,
+                    cancel: function () {
+                        $tr.removeClass('selected');
+                    }
+                });
+            }
+        });
+
+        //最小窗口
+        layerOpen();
 
         //相册
         layer.photos({
-            photos: '#layer_photos'
+            photos: '#layer_pht'
         });
     });
 });
